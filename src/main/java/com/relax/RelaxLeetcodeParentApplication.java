ca235276274d9297cf9496ebd652b52ad1cf1c25
_relax <ahaaaa04@163.com>
2024-08-20 14:55:32 +0800

    init: init repository

---
+++ src/main/java/com/relax/RelaxLeetcodeParentApplication.java
@@ -0,0 +1,13 @@
+package com.relax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxLeetcodeParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelaxLeetcodeParentApplication.class, args);
+    }
+
+}
